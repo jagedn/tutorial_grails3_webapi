cd5a7eacc91cd92a354ef12daa8a2f79658a4205
rafbermudez <rafa@rafbermudez.com>
2016-03-02 18:48:06 +0100

    Esqueleto inicial

---
+++ grails-app/init/tutorial_grails3_webapi/Application.groovy
@@ -0,0 +1,10 @@
+package tutorial_grails3_webapi
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
